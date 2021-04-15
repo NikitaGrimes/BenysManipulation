44d20bc57dd315a9d491b9308e08773ece2311dd
NikitaGrimes <nikita.rick@gmail.com>
2021-04-15 23:36:00 +0300

    init

---
+++ Манипуляции дяди Бени.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <string>   
+#include <sstream>
+#include <set>
+#include <iterator>
+
+using namespace std;
+
+int main()
+{
+	int n, m, u, v, w;
+	cin >> n >> m;
+	vector <vector<pair<int, int>>> Vertex(n);
+	vector <long long int> mass_vertex(n, -1);
+	for (int i = 0; i < m; i++)
+	{
+		cin >> u >> v >> w;
+		u--;
+		v--;
+		Vertex[u].push_back({ v, w });
+	}
+	mass_vertex[0] = 0;
+	int num;
+	set<pair<int, int>> next_steps;
+	next_steps.insert({ 0, 0 });
+	int j = 0;
+	while (!next_steps.empty())
+	{
+		auto step = --next_steps.end();
+		num = (*step).second;
+		next_steps.erase(step);
+		for (int i = 0; i < Vertex[num].size(); i++)
+		{
+			j = Vertex[num][i].first;
+			long long int new_mass = mass_vertex[num] + Vertex[num][i].second;
+			if (new_mass > mass_vertex[j])
+			{
+				mass_vertex[j] = new_mass;
+				next_steps.insert({ new_mass, j });
+			}
+		}
+	}
+	cout << mass_vertex[n - 1];
+}
